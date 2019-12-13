e1eb1ab416da407b76ff707f63eb35dd92a133b5
kangmin46 <kangmin789@naver.com>
2019-12-13 21:27:32 +0900

    feat : implement InstituteRepository

---
+++ src/main/java/com/example/demo/repository/InstituteRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.Entity.Institute;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InstituteRepository extends JpaRepository<Institute, Long> {
+}
