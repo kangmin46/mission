9211719bb5998f38ea628030e34078636318128a
kangmin46 <kangmin789@naver.com>
2019-12-15 02:43:08 +0900

    refactor : change Institution code field

--- src/main/java/com/example/demo/Institution.java
+++ src/main/java/com/example/demo/Institution.java
@@ -1,24 +1,25 @@
 package com.example.demo;
 
 import com.example.demo.exception.InstitutionNotFoundException;
+import com.example.demo.exception.InvalidInstitutionNameException;
 
 import java.util.Arrays;
 
 public enum Institution {
-    HOUSING_CITY_FUND("주택도시기금",1000),
-    KOOKMIN("국민은행",1001),
-    WOORI("우리은행",1002),
-    SHINHAN("신한은행",1003),
-    HANKOOK_CITY("한국시티은행",1004),
-    HANNA("하나은행",1005),
-    NONGHYUB_SOOHYUN("농협은행/수협은행",1006),
-    WAEWHAN("외환은행",1007),
-    ETC("기타은행",9999);
+    HOUSING_CITY_FUND("주택도시기금","hou00"),
+    KOOKMIN("국민은행","bhk00"),
+    WOORI("우리은행","bhk01"),
+    SHINHAN("신한은행","bhk02"),
+    HANKOOK_CITY("한국시티은행","bhk03"),
+    HANNA("하나은행","bhk04"),
+    NONGHYUB_SOOHYUN("농협은행/수협은행","bhk05"),
+    WAEWHAN("외환은행","bhk06"),
+    ETC("기타은행","bhk07");
 
     private final String name;
-    private final int code;
+    private final String code;
 
-    Institution(final String name, final int code) {
+    Institution(String name, String code) {
         this.name = name;
         this.code = code;
     }
@@ -30,7 +31,21 @@ public enum Institution {
             .orElseThrow(InstitutionNotFoundException::new);
     }
 
-    public int getCode() {
+    public static Institution ofCode(final String code) {
+        return Arrays.stream(Institution.values())
+            .filter(institution -> institution.getCode().equals(code))
+            .findAny()
+            .orElseThrow(InstitutionNotFoundException::new);
+    }
+
+    public static String checkValidInstitution(final String name) {
+        return Arrays.stream(Institution.values())
+            .filter(institution -> name.contains(institution.getName()))
+            .map(Institution::getName)
+            .findFirst().orElseThrow(InvalidInstitutionNameException::new);
+    }
+
+    public String getCode() {
         return code;
     }
 
