0fc9c579dda5cc48c992f5e4db720a450f403a79
kangmin46 <kangmin789@naver.com>
2019-12-15 02:50:19 +0900

    feat : implement Fund Entity

---
+++ src/main/java/com/example/demo/Entity/Fund.java
@@ -0,0 +1,78 @@
+package com.example.demo.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.ColumnResult;
+import javax.persistence.ConstructorResult;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedNativeQuery;
+import javax.persistence.SqlResultSetMapping;
+import java.util.Objects;
+@Entity
+public class Fund {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer year;
+
+    @Column(nullable = false)
+    private Integer month;
+
+    @Column(nullable = false)
+    private Integer amount;
+
+    @ManyToOne
+    private Institute institute;
+
+    public Fund(Integer year, Integer month, Integer amount, Institute institute) {
+        this.year = year;
+        this.month = month;
+        this.amount = amount;
+        this.institute = institute;
+    }
+
+    public Fund() {
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public Integer getMonth() {
+        return month;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public Institute getInstitute() {
+        return institute;
+    }
+
+    public String getInstituteName() {
+        return this.institute.getName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fund that = (Fund) o;
+        return Objects.equals(id, that.id) &&
+            Objects.equals(year, that.year) &&
+            Objects.equals(month, that.month) &&
+            Objects.equals(amount, that.amount) &&
+            Objects.equals(institute, that.institute);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, year, month, amount, institute);
+    }
+}
