8f80396d235acfee9a22ebaa7c31f81dbc9f3fba
kangmin46 <kangmin789@naver.com>
2019-12-15 02:46:45 +0900

    refactor : rename HousingFund to Fund

--- src/main/java/com/example/demo/repository/HousingFundRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.Entity.HousingFund;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HousingFundRepository extends JpaRepository<HousingFund, Long> {
-}
