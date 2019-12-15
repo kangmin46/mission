632753907f1529aa600a50b0522481556eed938d
kangmin46 <kangmin789@naver.com>
2019-12-15 18:11:28 +0900

    refactor : rename dtoMapper to objectMapper

--- src/main/java/com/example/demo/utils/ObjectMapper.java
+++ src/main/java/com/example/demo/utils/ObjectMapper.java
@@ -1,11 +1,13 @@
 package com.example.demo.utils;
 
 import com.example.demo.vo.StatisticsDto;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class DtoMapper {
+@Component
+public class ObjectMapper {
 
     public List<StatisticsDto> mapToStatisticsDto(List<Object[]> statistics) {
         return statistics.stream()
@@ -18,5 +20,13 @@ public class DtoMapper {
         int amount = Integer.parseInt(statistic[1].toString());
         int year= Integer.parseInt(statistic[2].toString());
         return new StatisticsDto(name, amount, year);
+    }
+
+    public List<Double> mapToLinearRegressionData(List<Object[]> recommendDatas) {
+        return recommendDatas.stream()
+            .map(re -> re[0])
+            .mapToDouble(re -> Double.parseDouble(re.toString()))
+            .boxed()
+            .collect(Collectors.toList());
     }
 }
