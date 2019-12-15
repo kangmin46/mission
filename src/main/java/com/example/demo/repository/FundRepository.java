d6054fe85394b7ea30428b75c449796a65e1d103
kangmin46 <kangmin789@naver.com>
2019-12-15 18:10:55 +0900

    feat : impliment findRecommendData method in FundRepository

--- src/main/java/com/example/demo/repository/FundRepository.java
+++ src/main/java/com/example/demo/repository/FundRepository.java
@@ -9,9 +9,15 @@ import java.util.List;
 public interface FundRepository extends JpaRepository<Fund, Long> {
     @Query(value =
         "select institute.name, sum(amount) as amount, year from fund\n" +
-        "inner join institute\n" +
-        "on institute.id = fund.institute_id\n" +
-        "group by institute.name, year;", nativeQuery = true)
+            "inner join institute\n" +
+            "on institute.id = fund.institute_id\n" +
+            "group by institute.name, year;", nativeQuery = true)
     List<Object[]> findStatistics();
 
+    @Query(value =
+        "select amount from fund\n" +
+            "inner join institute\n" +
+            "on fund.institute_id=institute.id\n" +
+            "where name= ?1", nativeQuery = true)
+    List<Object[]> findRecommendData(String name);
 }
